22cbfdc362f52f78789ce918b0280a9ea82f590c
Evgeniy Shestakov <Gennadith.bear@gmail.com>
2021-08-07 08:08:01 +0500

    homework-2

--- app/src/main/java/com/shestakov/pictureoftheday/MainActivity.kt
+++ app/src/main/java/com/shestakov/pictureoftheday/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.shestakov.pictureoftheday
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.shestakov.pictureoftheday.R
+import androidx.appcompat.app.AppCompatActivity
 import com.shestakov.pictureoftheday.picture.PictureOfTheDayFragment
+import com.shestakov.pictureoftheday.settings.SETTINGS_SHARED_PREFERENCES
+import com.shestakov.pictureoftheday.settings.THEME_RES_ID
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(
+            getSharedPreferences(SETTINGS_SHARED_PREFERENCES, MODE_PRIVATE)
+                .getInt(THEME_RES_ID, R.style.DefaultTheme)
+        )
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, PictureOfTheDayFragment.newInstance())
-                .commitNow()
+                .commitNowAllowingStateLoss()
         }
     }
 }
